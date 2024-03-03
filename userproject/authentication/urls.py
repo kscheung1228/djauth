86d26f5176fa409fc3e71ff493620b51404a430c
kscheung1228 <kscheung1228@gmail.com>
2024-03-03 23:28:06 +0800

    initial from https://dj-rest-auth.readthedocs.io/en/latest/installation.html#json-web-token-jwt-support-optional

---
+++ userproject/authentication/urls.py
@@ -0,0 +1,11 @@
+# authentication/urls.py
+
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    # URLs will come here
+    # path('dj-rest-auth/', include('dj_rest_auth.urls'))
+]
